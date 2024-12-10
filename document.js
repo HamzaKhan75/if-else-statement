170afd37a2357acd2e20def1fb356ae85016c688
hamza hussain/75 <154751641+HamzaKhan75@users.noreply.github.com>
2024-12-10 13:04:57 +0000

    Add files via upload

--- document.js
+++ document.js
@@ -1,7 +1,28 @@
-let num = 12;
+// let age = 17;
 
-if (num%2 === 0 ){
-       document.write("Even");
+// if(age >= 18){
+//     document.write("You Can Vote ")
+// } else 
+//  {
+//     document.write("You Cannot Vote")
+// }
+// document.write("<br>")
+// let num = 12;
+
+// if (num%2 === 0 ){
+//        document.write("Even");
+// } else{
+//        document.write("Odd");
+// }
+// document.write("<br>")
+
+let mode = prompt("Enter Your Favirot Colour");
+if(mode === "dark"){
+    document.write("black")
+} else if ( mode === "colour"){
+    document.write( "blue")
 } else{
-       document.write("Odd");
+    document.write( "white")
 }
+document.write("<br>")
+let 
